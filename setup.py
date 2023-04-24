eb425ba5f4ed31dadf3c34f2f5c7bd6dcaadbbf8
luis arthur <luisarthurlards03@gmail.com>
2023-04-24 20:46:09 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="auto-ambient",
-    version="0.2",
+    version="0.3",
     packages=["ambient", "ambient.tolls"],
     zip_safe=False,
     entry_points={
