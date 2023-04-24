b1f738dad823336ad869605b2fa66a969c60134f
luis arthur <luisarthurlards03@gmail.com>
2023-04-24 20:42:53 -0300

    fix: :bug: bug of the logging
    before that, the getFile breaks before the ambient was created

--- ambient/tolls/utils.py
+++ ambient/tolls/utils.py
@@ -1,17 +1,17 @@
 import logging
 
-logging.basicConfig(
-    handlers=[
-        logging.FileHandler(
-            filename="./logs/log_records.txt", encoding="utf-8", mode="a+"
-        )
-    ],
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%F %A %T",
-    level=logging.INFO,
-)
-
 
 def notify(text):
+
+    logging.basicConfig(
+        handlers=[
+            logging.FileHandler(
+                filename="./logs/log_records.txt", encoding="utf-8", mode="a+"
+            )
+        ],
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%F %A %T",
+        level=logging.INFO,
+    )
     print(text)
     logging.info(text)
