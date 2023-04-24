05277715f8305f11e3e03f26e9080c86593ce156
luis arthur <luisarthurlards03@gmail.com>
2023-04-24 20:48:54 -0300

    fix : flake 8

--- src/run.py
+++ src/run.py
@@ -1,5 +1,5 @@
 from botcity.core import DesktopBot
-import tags as tg
+import tags as tg  # noqa: E261, F401
 import ambient.tolls.clicks as cl
 
 cl_functions = [cl.find, cl.click, cl.clickIfPossible]
@@ -11,6 +11,6 @@ def run():
             find = cl.remove_self_necessity(self, cl.find)
             click = cl.remove_self_necessity(self, cl.click)
             clickIfPossible = cl.remove_self_necessity(self, cl.clickIfPossible)
-            nf = [find, click, clickIfPossible]
+            nf = [find, click, clickIfPossible]  # noqa: F841
 
     Bot.main()
