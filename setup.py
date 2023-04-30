9827433203af570d0687c3f1b92f7f7fb17ddf2e
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2023-04-30 16:56:03 -0300

    upgrade of setup.py with chatgpt by iap

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name="auto-ambient",
-    version="0.3",
-    packages=["ambient", "ambient.tolls", "ambient.models"],
+    version="0.3.1",
+    packages=["ambient", "ambient.tolls", "ambient.models", "src"],
     zip_safe=False,
     entry_points={
         "console_scripts": [
@@ -12,4 +12,10 @@ setup(
             "createAmbient=ambient.createAutoAmbient:main",
         ],
     },
+    install_requires=[
+        "PySimpleGUI==4.20.0",
+        "botcity==1.8.1",
+        "requests>=2.25.1"
+    ],
+    license='MIT',
 )
